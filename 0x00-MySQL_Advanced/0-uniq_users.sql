380e861fcd9cc7434b7acf3e26e067dce7c85edc
Obed101 <obedamoako92@gmail.com>
2022-04-21 19:52:22 +0000

    added comment to the filee

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- Creating a new table
+-- This 'users' table contain primary key, unique, etc
 CREATE TABLE IF NOT EXISTS users (
 id INT NOT NULL AUTO_INCREMENT,
 email VARCHAR(255) NOT NULL UNIQUE,
