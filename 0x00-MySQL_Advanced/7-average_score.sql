eb2b586b45de7bb7543da03b7faf01e54044be4d
Obed Amoako <obedamoako92@gmail.com>
2022-04-22 18:01:30 +0000

    modified average procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,8 +7,8 @@ CREATE PROCEDURE IF NOT EXISTS ComputeAverageScoreForUser (
 BEGIN
     DECLARE avg_score FLOAT;
     SET avg_score = (
-        SELECT AVG(score) FROM corrections AS _avg
-        WHERE _avg.user_id = user_id
+        SELECT AVG(score) FROM corrections
+        WHERE id = user_id
     );
     UPDATE users SET average_score = avg_score
     WHERE id = user_id;
