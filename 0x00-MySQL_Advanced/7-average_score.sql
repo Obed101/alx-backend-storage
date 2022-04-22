d7027055d055aa9fe2302ee9b81868439a1934f2
Obed Amoako <obedamoako92@gmail.com>
2022-04-22 18:19:10 +0000

    fixed issues with lost code

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,11 +5,18 @@ CREATE PROCEDURE IF NOT EXISTS ComputeAverageScoreForUser (
 )
 
 BEGIN
-    DECLARE avg_score FLOAT;
-    SET avg_score = (
-        SELECT AVG(score) FROM corrections
-        WHERE id = user_id
-    );
-    UPDATE users SET average_score = avg_score
+    DECLARE total_score FLOAT;
+    DECLARE no_of_projects INT;
+    DECLARE _average FLOAT;
+    SELECT SUM(score) INTO total_score FROM corrections
+    WHERE corrections.user_id = user_id;
+    -- Computing the average score...
+    SELECT COUNT(project_id) INTO no_of_projects
+    FROM corrections
+    WHERE corrections.user_id = user_id;
+    -- Updating table with new data...
+    SET _average = total_score / no_of_projects;
+    UPDATE users SET average_score = _average
     WHERE id = user_id;
 END$$
+DELIMITER ;
