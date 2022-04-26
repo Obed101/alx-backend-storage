2bbb0878ae17da76aa86f30ecb5204088257f80f
Obed Amoako <obedamoako92@gmail.com>
2022-04-26 21:18:35 +0000

    mongo insertion using python function

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Inserting mongo docs in collection """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts kwargs as Docs in mongo_collection """
+    return mongo_collection.insert_one(kwargs).inserted_id
