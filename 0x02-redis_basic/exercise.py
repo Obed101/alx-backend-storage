2a0ac5e6a2b26018c4232c83678d9fd3e3d928e3
Obed Amoako <obedamoako92@gmail.com>
2022-04-27 21:52:53 +0000

    added a fucntion that returns all history 1914

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,7 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
     """ Returns the history of all calls to Method and return values"""
 
@@ -35,17 +36,43 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable):
+    """ Returns all history on redis """
+    redis_ = redis.Redis()
+    func = method.__qualname__
+    n_of_calls = redis_.get(func)
+    try:
+        n_of_calls = n_of_calls.decode('utf-8')
+    except Exception:
+        n_of_calls = 0
+    print(f'{func} was called {n_of_calls} times:')
+
+    inputs = redis_.lrange(func + ":inputs", 0, -1)
+    outputs = redis_.lrange(func + ":outputs", 0, -1)
+
+    for num, output in zip(inputs, outputs):
+        try:
+            num = num.decode('utf-8')
+        except Exception:
+            num = ""
+        try:
+            output = output.decode('utf-8')
+        except Exception:
+            output = ""
+        print(f'{func}(*{num}) -> {output}')
+
+
 class Cache:
     """ This is the redis caching class """
     def __init__(self) -> None:
-        """This is the instance initializer"""
+        """This is the inputstance initializer"""
         self._redis = redis.Redis()
         self._redis.flushdb()
     
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Stores an instance and returns its id"""
+        """Stores an inputstance and returns its id"""
         id = str(uuid.uuid4())
         self._redis.set(data, id)
         return id
