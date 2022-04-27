60f5c442dce716f257298d73be379367ac27812a
Obed Amoako <obedamoako92@gmail.com>
2022-04-27 11:53:21 +0000

    created a class for redit objects

---
+++ 0x02-redis_basic/excercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+The first class file for the redis implementation
+"""
+from typing import Any
+import redis
+import uuid
+
+
+class Cache():
+    """ This is the redis caching class """
+    def __init__(self, client: redis.Redis()) -> None:
+        """This is the instance initializer"""
+        _client = client.flushdb()
+    
+    def store(self, data: Any) -> str:
+        """Stores an instance and returns its id"""
+        id = uuid.uuid4
+        redis.set(data, id)
+        return id
