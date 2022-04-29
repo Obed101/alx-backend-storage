ebfd20fecbbb9bd9393689d4fc5b7e8288b35b70
Obed Amoako <obedamoako92@gmail.com>
2022-04-29 14:55:47 +0000

    return query correctly

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,4 +3,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ This function returns all school topics that match topic """
-    return [school for school in mongo_collection.find({"topic": topic})]
+    return [school for school in mongo_collection.find({"topics": topic})]
