8201f6766c3561eca977b502205a9b8eba7b3810
Obed Amoako <obedamoako92@gmail.com>
2022-04-29 14:53:50 +0000

    listing all the matching topics

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" This module changes all topics of school docs"""
+
+def schools_by_topic(mongo_collection, topic):
+    """ This function returns all school topics that match topic """
+    return [school for school in mongo_collection.find({"topic": topic})]
