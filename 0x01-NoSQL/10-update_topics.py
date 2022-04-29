a99aeedefc32496823e9c8f1775267d1f791e97a
Obed Amoako <obedamoako92@gmail.com>
2022-04-29 14:23:23 +0000

    added school topics uptator

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This module changes all topics of school docs"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ This functions updates school's topics """
+    name_match = { "name": name }
+    new_topic = { "$set": { "topics", topics } }
+
+    return mongo_collection.update_many(name_match, new_topic)
